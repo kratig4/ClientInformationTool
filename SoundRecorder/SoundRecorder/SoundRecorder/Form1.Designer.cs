47c40acb50ba405e6a44d5c609ba4750db7a2e56
Krati Gupta <krati.g.gupta@sunlife.com>
2023-07-12 20:36:34 +0530

    changes

---
+++ SoundRecorder/SoundRecorder/SoundRecorder/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SoundRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(154, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload Image";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Upload PDF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(530, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(154, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(155, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Convert to text";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(342, 172);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 50);
+            this.textBox2.TabIndex = 7;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(154, 117);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Audio Record Start";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(299, 117);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(204, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Audio Record Stop";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private SaveFileDialog saveFileDialog1;
+        private TextBox textBox1;
+        private Button button5;
+        private TextBox textBox2;
+        private FileSystemWatcher fileSystemWatcher1;
+        private OpenFileDialog openFileDialog1;
+        private Button button6;
+        private Button button7;
+    }
+}
---
+++ SoundRecorder/SoundRecorder/SoundRecorder/Form1.cs
@@ -0,0 +1,233 @@
+using System.Diagnostics;
+using System;
+using System.Windows.Forms;
+using System.Collections;
+using System.Drawing;
+using Microsoft.VisualBasic;
+using System.Data;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Channels;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Speech.V1;
+using Grpc.Auth;
+using System;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using NAudio.Wave;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ClientInformationServices.Services.Impl;
+using ClientInformationServices.Services;
+
+namespace SoundRecorder
+{
+    public partial class Form1 : Form
+    {
+        int s = 0;
+        SpeechSynthesizer speechSynthesizerObj;
+        public WaveIn _waveIn = null;
+        public WaveFileWriter fileToWrite = null;
+        ImageToText _imageToText = new ImageToText();
+        PdfToText _pdfToText = new PdfToText();
+        public Form1()
+        {
+            InitializeComponent();
+            speechSynthesizerObj = new SpeechSynthesizer();
+
+        }
+
+        //[DllImport(@"C:\Users\yy23\source\repos\SoundRecorder\SoundRecorder\bin\Debug\net6.0-windows\winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int record(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            s = 0;
+            timer1.Enabled = false;
+            if (saveFileDialog1.FileName != "")
+            {
+                (new Microsoft.VisualBasic.Devices.Audio()).Play(saveFileDialog1.FileName);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            s++;
+            textBox1.Text = s.ToString();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            (new Microsoft.VisualBasic.Devices.Audio()).Stop();
+            record("close recsound", "", 0, 0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+
+            openFileDialog1.Filter = "wav (*.wav)|*.wav|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select audio file";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                textBox2.Text = "<Processing Audio Sample>";
+                string processfile = openFileDialog1.FileName;
+
+                GoogleCredential googleCredential;
+                StringBuilder sb1 = new StringBuilder();
+                using (Stream m = new FileStream(@"sidproject-2102-2360d6244d15.json", FileMode.Open))
+                    googleCredential = GoogleCredential.FromStream(m);
+                var client = new SpeechClientBuilder { ChannelCredentials = googleCredential.ToChannelCredentials() }.Build();
+
+                var response = client.Recognize(new RecognitionConfig()
+                {
+                    Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                    //SampleRateHertz = 16000,
+                    LanguageCode = "hu",
+                }, RecognitionAudio.FromFile(processfile));
+                foreach (var result in response.Results)
+                {
+                    foreach (var alternative in result.Alternatives)
+                    {
+                        sb1.Append((alternative.Transcript));
+                    }
+                }
+
+                textBox2.Text = sb1.ToString();
+            }
+        }
+
+
+
+        private void _waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (fileToWrite != null)
+            {
+                fileToWrite.Write(e.Buffer, 0, e.BytesRecorded); // writes bytes to the wav file
+                fileToWrite.Flush();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Enabled = false;
+            s = 0;
+            _waveIn.StopRecording();
+            fileToWrite.Dispose();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Filter = "Image files | *.png"; // file types, that will be allowed to upload
+            openFileDialog1.Multiselect = false; // allow/deny user to upload more than one file at a time
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                String path = openFileDialog1.FileName; // get name of file
+
+                var txt = _imageToText.call(path);
+                textBox2.Text = txt;
+            }
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            _waveIn = new WaveIn();
+            _waveIn.WaveFormat = new WaveFormat(44100, 1);
+            _waveIn.DataAvailable += _waveIn_DataAvailable; // event that keep listening mic
+
+            saveFileDialog1.Filter = "wav (*.wav)|*.wav|All files (*.*)|*.*";
+            saveFileDialog1.Title = "Save audio";
+            saveFileDialog1.ShowDialog();
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                timer1.Enabled = true;
+                timer1.Start();
+                fileToWrite = new WaveFileWriter(saveFileDialog1.FileName, _waveIn.WaveFormat);
+                _waveIn.StartRecording();
+            }
+        
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "PDF files | *.pdf"; // file types, that will be allowed to upload
+            openFileDialog1.Multiselect = false; // allow/deny user to upload more than one file at a time
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                String path = openFileDialog1.FileName; // get name of file
+
+                var txt = _pdfToText.call(path);
+                textBox2.Text = txt;
+            }
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer1.Enabled = false;
+            s = 0;
+            _waveIn.StopRecording();
+            fileToWrite.Dispose();
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+
+            openFileDialog1.Filter = "wav (*.wav)|*.wav|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select audio file";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                textBox2.Text = "<Processing Audio Sample>";
+                string processfile = openFileDialog1.FileName;
+
+                GoogleCredential googleCredential;
+                StringBuilder sb1 = new StringBuilder();
+                using (Stream m = new FileStream(@"sidproject-2102-2360d6244d15.json", FileMode.Open))
+                    googleCredential = GoogleCredential.FromStream(m);
+                var client = new SpeechClientBuilder { ChannelCredentials = googleCredential.ToChannelCredentials() }.Build();
+
+                var response = client.Recognize(new RecognitionConfig()
+                {
+                    Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                    //SampleRateHertz = 16000,
+                    LanguageCode = "hu",
+                }, RecognitionAudio.FromFile(processfile));
+                foreach (var result in response.Results)
+                {
+                    foreach (var alternative in result.Alternatives)
+                    {
+                        sb1.Append((alternative.Transcript));
+                    }
+                }
+
+                textBox2.Text = sb1.ToString();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
