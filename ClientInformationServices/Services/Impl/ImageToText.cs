6c111a2d6ac041b5e8ba8d7b57e369fe12df76cc
Krati Gupta <krati.g.gupta@sunlife.com>
2023-07-12 11:15:57 +0530

    Add project files.

---
+++ ClientInformationServices/Services/Impl/ImageToText.cs
@@ -0,0 +1,20 @@
+﻿using IronOcr;
+
+namespace ClientInformationServices.Services.Impl
+{
+    public class ImageToText 
+    {
+        public string call(string filePath)
+        {
+            var ocr = new IronTesseract();
+
+            using (var ocrInput = new OcrInput())
+            {
+                ocrInput.AddImage(filePath);
+
+                var ocrResult = ocr.Read(ocrInput);
+                return ocrResult.Text;
+            }
+        }
+    }
+}
---
+++ ClientInformationServices/Services/Impl/PdfToText.cs
@@ -0,0 +1,29 @@
+﻿using IronOcr;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientInformationServices.Services.Impl
+{
+    public class PdfToText 
+    {
+        public string call(string filePath)
+        {
+            var ocr = new IronTesseract();
+
+            using (var ocrInput = new OcrInput())
+            {
+                ocrInput.AddPdf(filePath);
+
+                var ocrResult = ocr.Read(ocrInput);
+                return ocrResult.Text;
+            }
+        }
+    }
+}
